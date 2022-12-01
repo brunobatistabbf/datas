f238122a1d5fe069072132e0f7e65a4d9026d73c
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2022-12-01 14:25:13 -0300

    bucket finalized

--- bucket/bucket.py
+++ bucket/bucket.py
@@ -1,25 +1,36 @@
-def bucketSort(array):
-    bucket = []
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i - 1
+        while j >= 0 and temp < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = temp
+    return arr
 
-    # Create empty buckets
-    for i in range(len(array)):
-        bucket.append([])
+def bucket_sort(arr):
+    comparacoes = 0
+    movimentacoes = 0
+    aux_mov = 0
+    result = []
+    no_of_buckets = 5
 
-    # Insert elements into their respective buckets
-    for j in array:
-        index_b = int(10 * j)
-        bucket[index_b].append(j)
+    buckets = [[] for i in range(no_of_buckets)]
 
-    # Sort the elements of each bucket
-    for i in range(len(array)):
-        bucket[i] = sorted(bucket[i])
+    max_elem, min_elem = max(arr), min(arr)
+    bucket_size = (max_elem - min_elem) / no_of_buckets
 
-    # Get the sorted elements
-    k = 0
-    for i in range(len(array)):
-        for j in range(len(bucket[i])):
-            array[k] = bucket[i][j]
-            k += 1
+    for element in arr:
+        index = int((element - min_elem) / bucket_size)
+        index -= 1 if element == max_elem else 0
+        comparacoes = comparacoes + 1
+        movimentacoes  = movimentacoes + 1
+        buckets[index].append(element)
+        movimentacoes  = movimentacoes + 1
 
+    for bucket in buckets:
+        result.extend(insertion_sort(bucket))
+        movimentacoes = movimentacoes + 1
 
-    return array
+    return result, movimentacoes, comparacoes
+
--- bucket/main.py
+++ bucket/main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import bucket
-from  bucket import bucketSort
+from  bucket import bucket_sort
 
 movimentacoes = 0
 comparacoes = 0
@@ -8,7 +8,7 @@ comparacoes = 0
 start = datetime.now()
 
 def dados():
-    arquivo = "dadosBucket/dados5.txt"
+    arquivo = "dadosBucket/dados100_mil.txt"
     with open(arquivo, "r") as arquivos:
         organizar = arquivos.read().replace(" ", "").replace("[", "").replace("]", "").split(",")
         dados = list(map(int, organizar))
@@ -16,22 +16,22 @@ def dados():
 
 arr = dados()
 
-array = bucketSort(arr)
+array, movimentacoes, comparacoes = bucket_sort(arr)
 
 end = datetime.now()
 
-arquivo = open('arquivosCriados/dados5Bucket.txt', 'x')
+arquivo = open('arquivosCriados/dados10milBucket.txt', 'x')
 arquivo.write("Bruno Batista Ferreira\n")
 arquivo.write("Bucket Sort")
 tempo = str(end - start)
 arquivo.write("\n")
 arquivo.write("Comparacoes: ")
-#comparacoesStr = str(comparacoes)
-#arquivo.write(comparacoesStr)
+comparacoesStr = str(comparacoes)
+arquivo.write(comparacoesStr)
 arquivo.write("\n")
-#movimentacoesStr = str(movimentacoes)
+movimentacoesStr = str(movimentacoes)
 arquivo.write("Movimentacoes: ")
-#arquivo.write(movimentacoesStr)
+arquivo.write(movimentacoesStr)
 arquivo.write("\n")
 arquivo.write(tempo)
 arquivo.write("\n")
